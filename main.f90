6561e5ca7599ca8bc29e562c40264bc07b7af420
Pablo Alcañiz <alcaniz.pablo@gmail.com>
2024-10-11 00:02:09 +0200

    dynamic type demonstrator

--- main.f90
+++ main.f90
@@ -1,5 +1,31 @@
-program pass_gen
+module data_type
     implicit none
+    public
+
+    type :: pass
+        character(len=40) :: service, user
+        character(:), allocatable :: password
+        integer :: order
+    end type
     
-    print*, "Hola, Mundo!"
+end module data_type
+
+program pass_gen
+    use data_type
+    implicit none
+
+    type(pass) :: prueba
+
+    prueba%order = 20
+    prueba%service = 'Amazon'
+    prueba%user = 'Pablo'
+
+    allocate(character(prueba%order) :: prueba%password)
+    prueba%password = '01234567890123456789'
+
+    print*, prueba%order
+    print*, prueba%service
+    print*, prueba%user
+    print*, prueba%password
+
 end program pass_gen
