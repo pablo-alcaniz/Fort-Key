fa85ca8de1c07e482fd04e32a31057cbc9835a35
Pablo Alcañiz <alcaniz.pablo@gmail.com>
2024-10-11 00:24:44 +0200

    add user inputed variables

--- main.f90
+++ main.f90
@@ -2,30 +2,31 @@ module data_type
     implicit none
     public
 
-    type :: pass
+    type :: pass_struct
         character(len=40) :: service, user
         character(:), allocatable :: password
         integer :: order
+        logical :: output = .true.
     end type
-    
+
 end module data_type
 
 program pass_gen
     use data_type
     implicit none
 
-    type(pass) :: prueba
+    type(pass_struct) :: pass
+    character(*), parameter :: dictionary = &
+    'abcdefghijklmnñopqrstuvwxyzABCDEFGHIJKLMNÑOPQRSTUVWXYZ000111222333444555666777888999!"#$%&()*+,-./:;<=>?@[\]^_`{|}~'
+    character(*), parameter :: special = ',;'
 
-    prueba%order = 20
-    prueba%service = 'Amazon'
-    prueba%user = 'Pablo'
+    print*, 'Type the service name:'
+    read*, pass%service
+    print*, 'Type your user:'
+    read*, pass%user
+    print*,'Type how long you want your password to be:'
+    read*, pass%order
 
-    allocate(character(prueba%order) :: prueba%password)
-    prueba%password = '01234567890123456789'
-
-    print*, prueba%order
-    print*, prueba%service
-    print*, prueba%user
-    print*, prueba%password
+    allocate(character(pass%order) :: pass%password)
 
 end program pass_gen
