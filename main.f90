a9b650885e0f5107a71e0e2e23c1728b06240f2e
Pablo Alcañiz <alcaniz.pablo@gmail.com>
2024-10-11 01:11:35 +0200

    basic pass gen feature

--- main.f90
+++ main.f90
@@ -4,7 +4,7 @@ module data_type
 
     type :: pass_struct
         character(len=40) :: service, user
-        character(:), allocatable :: password
+        character, allocatable :: password(:)
         integer :: order
         logical :: output = .true.
     end type
@@ -12,12 +12,15 @@ module data_type
 end module data_type
 
 program pass_gen
+    use iso_fortran_env
     use data_type
     implicit none
 
     type(pass_struct) :: pass
+    real(real64) :: random
+    integer :: i, a, ind
     character(*), parameter :: dictionary = &
-    'abcdefghijklmnñopqrstuvwxyzABCDEFGHIJKLMNÑOPQRSTUVWXYZ000111222333444555666777888999!"#$%&()*+,-./:;<=>?@[\]^_`{|}~'
+    'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0000111122223333444455556666777788889999!"#$%&()*+,-./:;<=>?@[\]^_`{|}~'
     character(*), parameter :: special = ',;'
 
     print*, 'Type the service name:'
@@ -27,6 +30,16 @@ program pass_gen
     print*,'Type how long you want your password to be:'
     read*, pass%order
 
-    allocate(character(pass%order) :: pass%password)
+    allocate(pass%password(pass%order))
+    
+    a = len(dictionary)
+
+    do i=1,pass%order
+        call random_number(random)
+        ind = 1+floor(a*random)
+        pass%password(i) = dictionary(ind:ind)
+    enddo
+
+    print*, pass%password
 
 end program pass_gen
